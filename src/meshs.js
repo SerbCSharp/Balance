ef33d7bf142fb4c279b6d6614f6207134da3a1fd
SerbCSharp <SerbCSharp@mail.ru>
2026-06-17 07:55:50 +0300

    17_06_26

--- src/meshs.js
+++ src/meshs.js
@@ -4,29 +4,21 @@ export const Cube = function (sector, positionX, positionY) {
   const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
   const cubeMaterial = new THREE.MeshBasicMaterial({
     side: THREE.DoubleSide,
+    name: sector,
   });
   const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
   cube.receiveShadow = true;
   cube.position.z = 1.1;
   cube.position.x = positionX;
   cube.position.y = positionY;
-
-  if (sector == "farmer") {
-    cube.name = "farmer";
-    cube.material.color = new THREE.Color("yellow");
-  }
-  if (sector == "bakery") {
-    cube.name = "bakery";
-    cube.material.color = new THREE.Color("blue");
-  }
-  if (sector == "shop") {
-    cube.name = "shop";
-    cube.material.color = new THREE.Color("red");
-  }
-  if (sector == "house") {
-    cube.name = "house";
-    cube.material.color = new THREE.Color("green");
-  }
+  Building(sector);
 
   return cube;
+};
+
+const Building = function (sector) {
+  if (sector == "farmer") cube.material.color = new THREE.Color("yellow");
+  if (sector == "bakery") cube.material.color = new THREE.Color("blue");
+  if (sector == "shop") cube.material.color = new THREE.Color("red");
+  if (sector == "house") cube.material.color = new THREE.Color("green");
 };
